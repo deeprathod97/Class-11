f21bdfbf92cda939b6d79e8dd8ac9cd0adbd1cbf
deeprathod97 <deeprathod9797@gmail.com>
2024-06-14 19:39:04 +0530

    class

---
+++ script.js
@@ -0,0 +1,23 @@
+// //1: target app element
+// //2: create a new div
+// //3: add class to new div
+// //4: write innerHTML in div
+// //5: Append cod to app
+
+
+// //1 :
+// const app = document.getElementById('app');
+
+// //2 :
+// const card = document.createElement('div');
+
+// //3 :
+// card.classList.add('card');
+
+// //4:
+
+// card.innerHTML = `
+// <h1>Hello word</h1>
+// <p>this is card</p>
+// `;
+// app.
